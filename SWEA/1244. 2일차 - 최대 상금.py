23eff843e96e6d01c8a28d40cc0f441bee77878a
heohyoyeong <heohyoyeong@gmail.com>
2023-09-26 17:32:33 +0900

    dfs example

--- SWEA/1244. 2일차 - 최대 상금.py
+++ SWEA/1244. 2일차 - 최대 상금.py
@@ -1,7 +1,32 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
-for test_case in range(1, T + 1):
-    data, chance = map(int,input().split())
-    a=1
+
+def dfs(index, count) :
+    global result
+    if count == int(cnt) :
+        result = max(int(''.join(data)), result)
+        return
+    for now in range(index, len(data)) :
+        for max_idx in range(now + 1, len(data)) :
+            if data[now] <= data[max_idx] :
+                data[now], data[max_idx] = data[max_idx], data[now]
+                dfs(now, count + 1)
+                data[now], data[max_idx] = data[max_idx], data[now]
+
+    if result == 0 and count < int(cnt) :
+        extra = (int(cnt) - count) % 2
+        # 짝수라면 그대로, 홀수라면 한 번 변경
+        if extra == 1 : # 홀수라면
+            data[-1], data[-2] = data[-2], data[-1]
+        dfs(index, int(cnt))
+
+t = int(input())
+
+for tc in range(1, t + 1) :
+    data, cnt = input().split()
+    data = list(data)
+    result = 0
+    dfs(0, 0)
+
+    print('#%d %d' % (tc, result))
