1cf004dbe237b0acd39a82b41244f2f476cea0b8
heohyoyeong <heohyoyeong@gmail.com>
2023-10-18 15:26:34 +0900

    swea advanced 1 clear

---
+++ SWEA/Course/advanced/5186. 1일차 - 이진수2.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def test(num):
+    result = []
+    n=1
+    while num != 0:
+        if num >= 2**(-1*n):
+            num -=2**(-1*n)
+            result.append(str(1))
+        else:
+            result.append(str(0))
+        n+=1
+        if num==0 or n>13:
+            break
+    if num!=0 or len(result)>13:
+        return "overflow"
+    else:
+        return "".join(result)
+
+
+T = int(input())
+for test_case in range(1, T + 1):
+    a = float(input())
+    result = test(a)
+    print(f"#{test_case} {result}")
