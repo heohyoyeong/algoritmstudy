845feeb4271b9ecb2238c457b71e88bae8fc6570
heohyoyeong <heohyoyeong@gmail.com>
2023-11-02 23:06:05 +0900

    swea advanced 3 clear

--- SWEA/Course/advanced/5203. 3일차 - 베이비진 게임.py
+++ SWEA/Course/advanced/5203. 3일차 - 베이비진 게임.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("input.txt", "r")
+sys.stdin = open("../../input.txt", "r")
 
 T = int(input())
 
@@ -8,24 +8,25 @@ for test_case in range(1, T+1):
     result = []
     A = []
     B = []
+
     for j in range(len(Array)):
-        cut = j%2
-        if cut ==0:
+        if len(result)!=0:
+            continue
+
+        if j%2 ==0:
             A.append(Array[j])
-            A.sort()
-            if len(A)>=3:
+            A = sorted(A)
+            if len(A) >= 3:
                 for i in range(len(A)-2):
-                    if (A[i] == A[i + 1] and A[i] == A[i + 2]) or (A[i] + 1 == A[i + 1] and A[i] + 2 == A[i + 2]) or (A[i] - 1 == A[i + 1] and A[i] - 2 == A[i + 2]):
+                    if (A[i] == A[i + 1] == A[i + 2]) or (A[i] == A[i + 1] -1 == A[i + 2] -2):
                         result.append(1)
-                        break
         else:
             B.append(Array[j])
-            B.sort()
+            B = sorted(B)
             if len(B) >= 3:
                 for i in range(len(B) - 2):
-                    if (B[i]==B[i+1] and B[i]==B[i+2]) or (B[i]+1 == B[i+1] and B[i]+2 == B[i+2]) or (B[i]-1 == B[i+1] and B[i]-2 == B[i+2]):
+                    if (B[i] == B[i+1] == B[i+2]) or (B[i] == B[i+1]-1 == B[i+2] - 2):
                         result.append(2)
-                        break
 
     if len(result)==0:
         print(f"#{test_case} 0")
